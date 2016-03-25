b2693721b81f8f9108a46aad7df813cbe1b91af7
jstratton <jamstratton@gmail.com>
2016-03-25 16:53:44 -0400

    Wrote word frequency counting code for a single document corpus.

---
+++ text_fill_model_v1.R
@@ -0,0 +1,52 @@
+# This file contains the code for a prototype text filling model.
+
+# Load req libraries
+library(tm)
+library(RWeka)
+library(SnowballC)
+
+# Load the sample corpus using the tm package
+docs <- DirSource(directory = paste0(getwd(), "/samples/compiled/"))
+texts <- VCorpus(docs)
+
+# Strip the whitespace from the documents
+texts <- tm_map(texts, stripWhitespace)
+
+# Find the 1-gram frequencies
+strsplit_space_tokenizer <- function(x)
+        unlist(strsplit(as.character(x), "[[:space:]]+"))
+
+# Set the parameters for termFreq
+ctrl <- list(tokenize = strsplit_space_tokenizer, tolower = FALSE)
+
+freq_1 <- termFreq(doc = texts[[1]], control = ctrl)
+
+# Find the 2-gram frequencies
+bigramtokenizer <- function(x){NGramTokenizer(x, Weka_control(min = 2, max = 2, 
+                                                              delimiters = " \n"))}
+
+ctrl <- list(tokenize = bigramtokenizer, tolower = FALSE)
+
+freq_2 <- termFreq(doc = texts[[1]], control = ctrl)
+
+# Find the 3-gram frequencies
+trigramtokenizer <- function(x){NGramTokenizer(x, Weka_control(min = 3, max = 3,
+                                                               delimiters = " \n"))}
+
+ctrl <- list(tokenize = trigramtokenizer, tolower = FALSE)
+
+freq_3 <- termFreq(doc = texts[[1]], control = ctrl)
+
+# Find the 4-gram frequencies
+tetragramtokenizer <- function(x){NGramTokenizer(x, Weka_control(min = 4, max = 4, 
+                                                                 delimiters = " \n"))}
+
+ctrl <- list(tokenize = tetragramtokenizer, tolower = FALSE)
+
+freq_4 <- termFreq(doc = texts[[1]], control = ctrl)
+
+# Sort the frequencies
+freq_1 <- sort(x = freq_1, decreasing = TRUE)
+freq_2 <- sort(x = freq_2, decreasing = TRUE)
+freq_3 <- sort(x = freq_3, decreasing = TRUE)
+freq_4 <- sort(x = freq_4, decreasing = TRUE)
