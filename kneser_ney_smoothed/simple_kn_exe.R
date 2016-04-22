8ad80ce2244d534b1acaee7b22a35c4712b93a0d
jstratton <jamstratton@gmail.com>
2016-04-22 15:47:16 -0400

    Added the trigram model.

--- kneser_ney_smoothed/simple_kn_exe.R
+++ kneser_ney_smoothed/simple_kn_exe.R
@@ -14,9 +14,11 @@ D = 0.75
 
 # This function makes a prediction using a binary model based on the history
 bigram_model <- function(history){
+        # Extract the final word from the history
+        final <- history[nrow(history), grams]
         # Check if the previous word is w/in our observed bigrams
         setkey(bigrams, big_first)
-        if(nrow(bigrams[list(history[length(grams)])]) < 1){
+        if(bigrams[list(final), all(is.na(big_freq))]){
                 # Compute the Kneser-Ney probabilities for each unigram
                 P_KN0 <- copy(monograms)
                 
@@ -43,8 +45,8 @@ bigram_model <- function(history){
                 setkey(P_KN2, completion)
                 
                 # Fill the data table with any counts that we have seen
-                P_KN2[list(bigrams[list(history[length(grams)]), big_second]), 
-                      count := bigrams[list(history[length(grams)]), big_freq]
+                P_KN2[list(bigrams[list(final), big_second]), 
+                      count := bigrams[list(final), big_freq]
                      ]
                 
                 # Compute the total number of counts for the given history
@@ -55,7 +57,7 @@ bigram_model <- function(history){
                 # Find the first order probabilities
                 setkey(bigrams, big_second)
                 P_KN1 <- data.table(mono_word = monograms[,map],
-                                    P1 = bigrams[, max(sum(big_freq > 0) - D),
+                                    P1 = bigrams[, max(sum(big_freq > 0) - D, 0),
                                                  by = big_second]$V1)
                 P_KN1[, P1 := P1/nrow(bigrams)] # Normalize by the no. of bigrams
                 P_KN1[, P1 := P1 + D/nrow(bigrams)] # Include the zero order term
@@ -78,15 +80,75 @@ trigram_model <- function(history){
         # Check to see if the history is w/in our observed trigrams
         setkey(trigrams, tri_first, tri_second) # Sort trigrams to allow binary searching
         
-        first <- history[length(history) - 1] # Store the penultimate word of the history
-        second <- history[length(history)] # store the final word of the history
+        first <- history[nrow(history) - 1, grams] # Store the penultimate word
+        second <- history[nrow(history), grams] # store the final word of the history
         
-        if(nrow(trigrams[list(first,second)]) == 0){
+        if(trigrams[list(first,second), all(is.na(tri_freq))]){
                 # If the history wasn't observed, switch to the bigram model
                 bigram_model(history)
         }
         else{
+                # Initialize a data table with an entry for every possible continuation
+                P_KN3 <- data.table(completion3 = monograms[, min(map)]:monograms[, max(map)],
+                                    count = 0L,
+                                    P3 = 0.0)
+                setkey(P_KN3, completion3)
                 
+                # Fill the data table with any counts that we have seen
+                P_KN3[list(trigrams[list(first, second), tri_third]), 
+                      count := trigrams[list(first, second), tri_freq],
+                      ]
+                
+                # Compute the total number of counts for the given history
+                total_counts <- P_KN3[,sum(count)]
+                # Find the number of unique completions associated with the history
+                unique_counts <- P_KN3[,sum(count > 0)]
+                
+                # Find the probability for each completion based on counts
+                P_KN3[,P3 := max(count - D, 0)/total_counts, by = completion3]
+                
+                # Find the 1st order probs
+                setkey(bigrams, big_second) # Sort the bigram table properly
+                P_KN1 <- data.table(completion1 = monograms[,map],
+                                    P1 = bigrams[, max(sum(big_freq > 0) - D, 0),
+                                                 by = big_second]$V1)
+                setkey(P_KN1, completion1)
+                
+                P_KN1[, P1 := P1/nrow(bigrams)] # Normalize by the no. of bigrams
+                P_KN1[, P1 := P1 + D/nrow(bigrams)] # Include the zero order term
+                
+                # Find the 2nd order probs
+                # Initialize a data table to store the 2nd order probs
+                P_KN2 <- data.table(completion2 = monograms[, min(map)]:monograms[, max(map)],
+                                    P2 = 0.0)
+                setkey(P_KN2, completion2) # set P_KN2's key to allow bin sorting
+                
+                # Order the trigrams to allow quick subsetting
+                setkey(trigrams, tri_second)
+                
+                # Find N1+(*,w_i-1,w_i), note that w_i-1 is fixed
+                N1 <- trigrams[list(second), sum(tri_freq > 0), by = tri_third]
+                N1_total <- N1[,sum(V1)] # Find the total number of phrases containing w_i-1
+                
+                # Find N1+(w_i-1, *)
+                setkey(bigrams, big_first)
+                N1_preceding <- bigrams[list(second), sum(big_freq > 0)]
+                
+                # Map each N1+ value onto its corresponding word
+                P_KN2[list(N1[,tri_third]), P2 := N1[,V1]]
+                # Find the first term of the probability
+                P_KN2[, P2 := max(P2 - D, 0)/N1_total, by = completion2]
+                # Interpolate with the first order term
+                P_KN2[, P2 := P2 + D*N1_preceding*P_KN1[,P1]/N1_total]
+                
+                # Interpolate P3 with P2 (and by extension P1)
+                P_KN3[, P3 := P3 + D*unique_counts*P_KN2[,P2]/total_counts]
+                
+                P_KN3[, P3 := log(P3)] # Convert to log probs
+                
+                # Output a prediction
+                P_KN3 <- P_KN3[order(-P3)]
+                monograms[list(P_KN3[,head(completion3)])]
         }
 }
 
@@ -102,13 +164,13 @@ word_guesser <- function(history = ""){
         
         # Convert each word to a numeric using the map
         setkey(monograms, word)
-        history[, grams := monograms[grams, map]]
+        history[, grams := monograms[list(grams), map]]
         
         # Sort the monograms based on the map
         setkey(monograms, map)
         
         # Use a bigram model if we only have one word in the history
-        if(length(history) < 2){
+        if(nrow(history) < 2){
                 # Run the bigram model
                 bigram_model(history)
         }
