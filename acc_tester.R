0ed9e1e81323f396e5590ae94e91ffd5b0c0e602
jstratton <jamstratton@gmail.com>
2016-07-06 22:01:13 -0400

    Unsplit the question phrases.

--- acc_tester.R
+++ acc_tester.R
@@ -9,7 +9,7 @@ texts <- list(twit = readLines(con = paste0(dir, "/en_US.twitter.txt"),
                                encoding = "UTF-8")
 )
 
-texts <- unlist(texts)
+texts <- unlist(texts, use.names = FALSE)
 
 # Remove the samples that were included in the training set
 pcnt <- 10
@@ -32,7 +32,10 @@ texts <- lapply(X = texts, FUN = function(x){unlist(strsplit(as.character(x), "[
 questions <- lapply(texts, FUN = function(x){x[1:(length(x) - 1)]})
 answers <- lapply(texts, FUN = function(x){x[length(x)]})
 
+# Put the sentences back together again for the questions
+questions <- lapply(questions, FUN = function(x){paste0(x, collapse = " ")})
+
 # Test the accuracy of a given model
-acc_test <- function(model = function(){"a"}){
+acc_test <- function(model = function(){"The"}){
         
 }
