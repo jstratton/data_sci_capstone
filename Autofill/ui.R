785541e6251032fc5c4c0e1d856a468809f5ff01
jstratton <jamstratton@gmail.com>
2016-07-14 17:48:19 -0400

    Setup a basic UI for my app.

---
+++ Autofill/ui.R
@@ -0,0 +1,33 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(pageWithSidebar(
+        
+        # Title
+        headerPanel("Text Completer App"),
+        
+        # Text Entry Field
+        sidebarPanel(
+                # Create a text input
+                textInput("history", "Enter your Phrase Here:", ""),
+                
+                # Add a submit button
+                submitButton(text = "Submit")
+        ),
+        
+        # Output result.
+        mainPanel(
+                h3("This is the guessed word:"),
+                
+                verbatimTextOutput("answer")
+        )
+))
